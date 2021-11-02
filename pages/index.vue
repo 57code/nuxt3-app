c34c59393a89ff1a269d02f33e961725e2d7fbe8
57code <yt0379@sohu.com>
2021-11-02 12:01:56 +0800

    约定路由

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Index Page</h1>
-    <NuxtLink to="/detail">Detail</NuxtLink>
+    <NuxtLink to="/detail">Detail</NuxtLink> | 
+    <NuxtLink to="/user-admin/1">/user-admin/1</NuxtLink> | 
+    <NuxtLink to="/parent">/parent/child</NuxtLink> | 
   </div>
 </template>
