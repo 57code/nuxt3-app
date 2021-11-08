6a5d71a0d1c6874805b87873639917b6bbd10bc0
57code <yt0379@sohu.com>
2021-11-08 16:20:11 +0800

    布局

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
     <NuxtLink to="/detail">Detail</NuxtLink> | 
     <NuxtLink to="/user-admin/1">/user-admin/1</NuxtLink> | 
     <NuxtLink to="/parent">/parent/child</NuxtLink> | 
+    <NuxtLink to="/helloworld">/helloworld</NuxtLink> | 
   </div>
 </template>
