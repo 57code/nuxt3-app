85c0c06d43136c8e05c9c7acefe8667f81b7746e
57code <yt0379@sohu.com>
2021-11-16 08:55:39 +0800

    组件自动导入

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
+    <TheHeader></TheHeader>
     通用布局页，default.vue:
     <slot />
+    <the-body></the-body>
+    <TheFooter></TheFooter>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1>Index Page</h1>
-    <NuxtLink to="/detail">Detail</NuxtLink> | 
-    <NuxtLink to="/user-admin/1">/user-admin/1</NuxtLink> | 
-    <NuxtLink to="/parent">/parent/child</NuxtLink> | 
-    <NuxtLink to="/helloworld">/helloworld</NuxtLink> | 
+    <!-- 组件自动导入 -->
+    <BaseFooButton></BaseFooButton>
+    <!-- 组件懒加载 -->
+    <h1>Mountains</h1>
+    <LazyMountainsList v-if="show" />
+    <button v-if="!show" @click="show = true">显示列表</button>
+
+    <NuxtLink to="/detail">Detail</NuxtLink> |
+    <NuxtLink to="/user-admin/1">/user-admin/1</NuxtLink> |
+    <NuxtLink to="/parent">/parent/child</NuxtLink> |
+    <NuxtLink to="/helloworld">/helloworld</NuxtLink> |
   </div>
 </template>
+<script setup>
+import { ref } from "vue";
+const show = ref(false);
+</script>
