432f6c6fbb28b0905365f253d679f8a3530092c8
57code <yt0379@sohu.com>
2021-11-29 07:47:53 +0800

    data

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@
     <LazyMountainsList v-if="show" />
     <button v-if="!show" @click="show = true">显示列表</button>
 
+    <!-- 待办列表 -->
+    <div v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" v-model="todo.completed" />
+      <strong>{{ todo.title }}</strong>
+    </div>
+
     <NuxtLink to="/detail">Detail</NuxtLink> |
     <NuxtLink to="/user-admin/1">/user-admin/1</NuxtLink> |
     <NuxtLink to="/parent">/parent/child</NuxtLink> |
@@ -17,4 +23,18 @@
 <script setup>
 import { ref } from "vue";
 const show = ref(false);
+
+// useAsyncData使用
+const { data: todos } = await useAsyncData("todos", () => $fetch("/api/todo"), {
+  transform(input) {
+    return input.data;
+  },
+  // pick: ['data'],
+});
+console.log(todos.value);
+// useFetch使用
+// const { data: todos } = await useFetch("/api/todo", {
+//   // pick: ["id", "title", "completed"],
+// });
+
 </script>
